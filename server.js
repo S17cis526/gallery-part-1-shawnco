1f4b1d4a22b6f98ea33940710b3c5ed10f6b85a9
Shawn Contant <shawnco@ksu.edu>
2017-02-02 14:19:12 -0600

    2 feb 2017

--- server.js
+++ server.js
@@ -12,14 +12,39 @@ var port = 3002;
 
 var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
 
+// Resource files for the images
+var ace = fs.readFileSync('images/ace.jpg');
+var bubble = fs.readFileSync('images/bubble.jpg');
 var chess = fs.readFileSync('images/chess.jpg');
 var fern = fs.readFileSync('images/fern.jpg');
-var ace = fs.readFileSync('images/ace.jpg');
+var mobile = fs.readFileSync('images/mobile.jpg');
+
+// Resource for the stylesheet
 var stylesheet = fs.readFileSync('gallery.css');
 
+// Retrieve all image file names
+function getImageNames(callback){
+	fs.readdir('images', function(err, fileNames){
+		if(err){
+			callback(err, undefined);
+		}else{
+			callback(false, fileNames);
+		}
+	});
+}
+
+// Convert image name to tags
+function imageNamesToTags(fileNames){
+	return fileNames.map(function(fileName){
+		return `<img src="${fileName}" alt="${fileName}" />`;
+	});
+}
+
+
+// Give the image
 function serveImage(filename, req, res)
 {
-	var body = fs.readFile('images/' + filename, function(err, body)
+	var data = fs.readFile('images/' + filename, function(err, data)
 	{
 		if (err)
 		{
@@ -30,57 +55,60 @@ function serveImage(filename, req, res)
 			return;
 		}
 		res.setHeader('Content-Type', 'image/jpeg');
-		res.end();
+		res.end(data);
 	});
 
+}
+
+// Compiles the gallery HTML together
+function buildGallery(imageTags){
+      var gHtml = imageNames.map(function(fileName)
+      {
+       	return '<img src="' + fileName + '" />';
+      }).join(' ');            
+		var html = '<!doctype html>';
+		html += '<html><head><title>Dynamic Page</title>';
+      html += '<link rel="stylesheet" href="gallery.css" type="text/css" />';
+      html += '</head><body>';
+		html += '<h1>Gallery</h1>';
+      html += gHtml;
+      html += '<h1>Hai</h1> Time is  ' + Date.now();
+		html += '</body></html>';
+		return html;
+}
+
+// Serve the gallery page
+function serveGallery(req, res){
+		getImageNames(function(err, imageNames){
+			if(err) {
+				console.log(err);
+				res.statusCode = 500;
+				res.statusMessage = 'Server error';
+				res.end();
+				return;
+			}
+			res.setHeader('Content-Type', 'text/html');
+			res.end(buildGallery(imageNames));			
+		});
 }
 
 var server = http.createServer((req, res) =>
 {
 	switch (req.url)
 	{
+		// Gallery page
+		case '/':
 		case '/gallery':
-      var gHtml = imageNames.map(function(fileName)
-      {
-        return '<img src="' + fileName + '" />';
-      }).join(' ');
-			var html = '<!doctype html>';
-			html += '<html><head><title>Dynamic Page</title>';
-      html += '<link rel="stylesheet" href="general.css" type="text/css" />';
-      html += '</head><body>';
-			html += '<h1>Gallery</h1>';
-//			html += '<img src="images/ace.jpg" alt="A fishing ace at work">';
-      html += gHtml;
-      html += '<h1>Hai</h1> Time is  ' + Date.now();
-			html += '</body></html>';
-			res.setHeader('Content-Type', 'text/html');
-			res.end(html);
+			serveGallery(req, res);
 			break;
-		case '/chess':
-  			serveImage('chess.jpg', req, res);
-  			res.end(chess);
-			break;
-		case '/fern':
-		case '/fern/':
-		case '/fern.jpg':
-		case '/fern/jpeg':
-			serveImage('fern.jpg', req, res);
-			res.end(fern);
-			break;
-		case '/ace':
-    case '/ace.jpg':
-    case '/ace.jpeg':
-			serveImage('ace.jpg', req, res);
-      res.end(ace);
-			break;
-    case '/gallery.css':
-      res.setHeader('Content-Type', 'text/css');
-      res.end(stylesheet);
-      break;
+			
+		// Gallery CSS
+   	case '/gallery.css':
+      	res.setHeader('Content-Type', 'text/css');
+      	res.end(stylesheet);
+      	break;		
 		default:
-			res.statusCode = 404;
-			res.statusMessage	= 'I am a teapot';
-			res.end('Could not find it!');
+			serveImage(req.url, req, res);
 			break;
 	};
 
