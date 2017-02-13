f9a4c6dd817a47c47eb2d071ef9a23850b137960
Shawn Contant <shawnco@ksu.edu>
2017-02-13 09:31:21 -0600

    Catching up

--- server.js
+++ server.js
@@ -19,8 +19,54 @@ var chess = fs.readFileSync('images/chess.jpg');
 var fern = fs.readFileSync('images/fern.jpg');
 var mobile = fs.readFileSync('images/mobile.jpg');
 
+// Config file
+var configJsonString = fs.readFileSync('config.json', {'encoding': 'utf-8'});
+var config = JSON.parse(configJsonString);
+var title = config['title'];
+
 // Resource for the stylesheet
 var stylesheet = fs.readFileSync('gallery.css');
+
+function saveConfig(){
+	var data = JSON.stringify(config);
+	fs.writeFile('config.json', data, function(err){
+		console.error('Error saving config file', err);
+	});
+}
+
+// Handle errors
+function handleError(req, res, err){
+	console.err(err, req, res);
+	res.writeHead(500, {'Content-Type': 'text/html'});
+	res.end('Server Error');
+}
+
+function handleRequest(req, res){
+	if(req.url == '/') buildGallery(req, res);
+	else if(req.substring(0,8) == '/images/') sendImage(req, res);
+	else {
+		res.writeHead(404, {'Content-Type': 'text/html'});
+		res.end('not found');
+	}
+}
+
+function enumerateImageFiles(req, res){
+	fs.readdir('images', function(err, items){
+		if(err) { handleError(req, res, err); return; }
+		var toProcess = items.length;
+		var files = [];
+		items.each(function(item){
+			item.stats(function(err,stats){
+				if(err) console.error(err);
+				else if(stats.isFile()) files.push(item);
+				toProcess--;
+				if(toProcess == 0) buildGallery(req, res, files);
+			}
+		}
+	});
+}		
+
+
 
 // Retrieve all image file names
 function getImageNames(callback){
@@ -44,7 +90,7 @@ function imageNamesToTags(fileNames){
 // Give the image
 function serveImage(filename, req, res)
 {
-	var data = fs.readFile('images/' + filename, function(err, data)
+	/*var data = fs.readFile('images/' + filename, function(err, data)
 	{
 		if (err)
 		{
@@ -56,13 +102,25 @@ function serveImage(filename, req, res)
 		}
 		res.setHeader('Content-Type', 'image/jpeg');
 		res.end(data);
+	});*/
+	var filename = './' + req.url;
+	fs.stat(filename, function(err, stats){
+		if(err){handleError(req, res, err); return;}
+		if(stats.isFile()){
+			fs.readFile(filename, function(err, file){
+				if(err){handleError(req, res, err); return;}
+				res.writeHead(200, {'Content-Type': 'text/' + filename.split('.').last});
+				res.end(file);
+			});
+		}
 	});
+}
 
 }
 
 // Compiles the gallery HTML together
-function buildGallery(imageTags){
-      var gHtml = imageNames.map(function(fileName)
+function buildGallery(req, res, images){
+      /*var gHtml = imageNames.map(function(fileName)
       {
        	return '<img src="' + fileName + '" />';
       }).join(' ');            
@@ -74,12 +132,48 @@ function buildGallery(imageTags){
       html += gHtml;
       html += '<h1>Hai</h1> Time is  ' + Date.now();
 		html += '</body></html>';
-		return html;
+		return html;*/
+	images.map(function(file){
+		return "<img src='/images/" + file "'/>";
+	}).join("\n");
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(
+		'<!doctype html>' +
+		'<html><head><title>Photo Gallery</title></head><body>' +
+		'<h1>Photo Gallery</h1>' + images + '</body></html>');
 }
 
 // Serve the gallery page
 function serveGallery(req, res){
-		getImageNames(function(err, imageNames){
+	/*if(req.query.title){
+		title = query.title;
+		config.title = query.title;
+		saveConfig();
+	}*/
+	if(data && data.title){
+		title = data.title;
+		config.title = data.title;
+		saveConfig();
+	}
+
+	fs.readdir('images', function(err, files){
+		if(err){
+			console.error('Error in serveGallery', err);
+			res.writeHead(500, {'Content-Type': 'text/html'});
+			res.end('Server Error');
+			return;
+		}
+		var imageTags = files.filter(function(file){
+			return fs.statSync('images/'+file).isFile();
+		}).map(function(file){
+			return "<img src='/images/"+file+"'/>";
+		}).join("\n");
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(
+			'<!doctype><html><head><title>' + title + '</title><link href="gallery.css" type="text/css" rel="stylesheet"/></head><body><h1>' + title + '</h1>' + generateTitleForm() + '<div class="gallery">' + imageTags + '</div></body></html>.');
+	});
+}
+		/*getImageNames(function(err, imageNames){
 			if(err) {
 				console.log(err);
 				res.statusCode = 500;
@@ -89,7 +183,28 @@ function serveGallery(req, res){
 			}
 			res.setHeader('Content-Type', 'text/html');
 			res.end(buildGallery(imageNames));			
-		});
+		});*/
+}
+
+function generateTitleForm(){
+	return '<form action="/" method="POST"><input type="text" name="title" value="' + title + '"><input type="submit" value="Save Changes"/></form>';
+
+function parsePostData(req, res, callback){
+	if(request.method.toUpperCase() != 'POST') return callback(req, res);
+	var body = '';
+	req.on('data', function(data){
+		body += data;
+		if(body.length > config.maxUploadSizeInMB * Math.pow(10,6))
+			request.connection.destroy();
+	});
+	req.on('end', function(){
+		var data = qs.parse(body);
+		callback(req, res, query, data);
+	});
+}
+
+function generateImageForm(){
+	'<form enctype="multipart/form-data" action="/" method="POST"><input type="file" name="image"><input type="submit" value="Upload Image"></form>';
 }
 
 var server = http.createServer((req, res) =>
@@ -113,7 +228,25 @@ var server = http.createServer((req, res) =>
 	};
 
 });
-server.listen(port, () =>
+/*server.listen(port, () =>
 {
 	console.log('Listening on port ' + port);
-});
+});*/
+var server = new http.Server(function(req, res){
+	var resource = req.url.split('?')[0];
+	req.query = querystring.parse(req.url.split('?')[1]);
+	switch(resource){
+		case '/':
+			parsePostData(req, res, serveGallery);
+			break;
+		case '/favicon.ico':
+			req.writeHead(400);
+			req.end();
+			break;
+		case '/gallery.css';
+			serveCSS(req, res);
+			break;
+		default:
+			serveImage(req, res);
+			break;
+}).listen(80);
