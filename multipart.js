1f938c42f45216a50b773d345096fe6d04087097
Shawn Contant <shawnco@WIN2.CS.KSU.EDU>
2017-02-14 13:06:43 -0600

    catch up

--- multipart.js
+++ multipart.js
@@ -6,6 +6,7 @@
  "use strict";
 
  module.exports = multipart;
+  const CRLF = Buffer.from([0x0D, 0x0A]);
   const DOUBLE_CRLF = Buffer.from([0x0D, 0x0A, 0x0D, 0x0A]);
 
  /**
@@ -16,9 +17,36 @@
   * request and response.
   */
   function multipart(req, res, next){
+	var chunks = [];
     req.on('error', function(){
       console.error(err);
-    })
+	  statusCode = 500;
+	  statusMessage = 'Server error';
+    });
+	req.on('data', function(chunk){
+		chunks.push(chunk);
+	});
+	req.on('end', function(){
+		var body = Buffer.concat(chunks);
+		var match = /boundary=(.+);?/.exec(req.headers['content-type']);
+		if(match && match[1]){
+			processBody(body, match[1], function(err, content){
+				if(err){
+					console.log(err);
+					res.statusCode = 500;
+					res.statusMessage = 'Server error';
+					res.end();
+				}
+				req.body = contents;
+				next(req, res);
+			});
+		}else{
+			console.error('No multipart boundary defined');
+			req.statusCode = 400;
+			req.statusMessage = 'Malformed multipart request';
+			res.end();
+		}
+	});
   }
 
 /**
@@ -27,8 +55,16 @@
   * of key/value pairs; if the content is a file, value will be an object with
   * properties filename, contentType, and data.
   */
-  function processBody(buffer, boundary){
-    var contents = [];
+  function processBody(buffer, boundary, callback){
+	var formData = {};
+	splitContentParts(buffer, boundary).forEach(function(content){
+		parseContents(content, function(err, parts){
+			if(err) return callback(err);
+			formData[parts[0]] = parts[1];
+		});
+	});
+	callback(false, formData);
+    /*var contents = [];
     var start = buffer.indexOf(boundary) + boundary.length + 2;
     var end = buffer.indexOf(boundary, start);
 
@@ -44,8 +80,20 @@
         parsedContents[tuple[0]] = tuple[1]l
       });
     });
-    return parsedContents;
+    return parsedContents;*/
   }
+  
+function splitContentParts(buffer, boundary){
+	var parts = [];
+	var start = buffer.indexOf('--' + boundary) + boundary.length + 2;
+	var end = buffer.indexOf(boundary, start);
+	while(end > start){
+		parts.push(buffer.slice(start,end));
+		start = end + boundary.length;
+		end = buffer.indexOf(boundary, start);
+	}
+	return parts;
+}
 
   /**
    * @function parseContent
@@ -55,22 +103,23 @@
   function parseContent(content, callback){
     var index = content.indexOf(DOUBLE_CLRF);
     var head = content.slice(0, index).toString();
-    var body = content.slice(index + 4);
-    var name = /name="([\w\d\-_]+)"/.exec(head);
-    var filename = /filename="([\w\d\-_\.]+)"/.exec(head);
-    var contentType = /Content-Type: ([\w\d\/]+)/.exec(head);
-
-    if(!name) return callback('Content without name');
-
-    if(filename){
-      // we have a file
-      callback(false, [name[1], {
-          filename: filename[1],
-          contentType: (contentType) ? contentType[1] : 'application/octet',
-          data: body
-        }]);
-    }else{
-      // We have a value
-      callback(false, name[1], body.toString()]);
-    }
+    var body = content.slice(index + 4, buffer.length);
+	var headers = {};
+	head.split(CRLF).forEach(function(line){
+		var parts = line.splot(': ');
+		var key = parts[0].toLowerCase();
+		var value = parts[1];
+		headers[key] = value;
+	});
+	
+    var name = /name="([\w\d\-_]+)"/.exec(headers['content-disposition']);
+	if(!name) return callback('No name in multipart content headers');
+	var filename = /filename="([^\\/:\*\?"<>\|]+)"/.exec(headers['content-disposition']);
+	if(filename){
+		var contentType = headers['content-type'];
+		if(!contentType) contentType = 'application/octet-stream';
+		calllback(false, [name[1], {filename: filename[1], contentType: contentType, data: body}]);
+	}else{
+		callback(false, [name[1], buffer.toString()]);
+	}
   }
--- server.js
+++ server.js
@@ -6,11 +6,13 @@
  * simple photo gallery web app.
  */
 
+var multipart = require('./multipart');
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 var port = 3002;
 
-var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
+var config = JSON.parse(fs.readFileSync('config.json'));
 
 // Resource files for the images
 var ace = fs.readFileSync('images/ace.jpg');
@@ -44,37 +46,39 @@ function imageNamesToTags(fileNames){
 // Give the image
 function serveImage(filename, req, res)
 {
-	var data = fs.readFile('images/' + filename, function(err, data)
-	{
-		if (err)
-		{
-			console.log(err);
-			res.statusCode = 500;
-			res.statusMessage = 'I dun goofd';
-			res.end('meh');
-			return;
-		}
-		res.setHeader('Content-Type', 'image/jpeg');
-		res.end(data);
-	});
-
+  fs.readFile('images/' + decodeURIComponent(fileName), function(err, data){
+    if(err) {
+      console.error(err);
+      res.statusCode = 404;
+      res.statusMessage = "Resource not found";
+      res.end();
+      return;
+    }
+    res.setHeader('Content-Type', 'image/*');
+    res.end(data);
+  });
 }
 
 // Compiles the gallery HTML together
 function buildGallery(imageTags){
-      var gHtml = imageNames.map(function(fileName)
-      {
-       	return '<img src="' + fileName + '" />';
-      }).join(' ');            
-		var html = '<!doctype html>';
-		html += '<html><head><title>Dynamic Page</title>';
-      html += '<link rel="stylesheet" href="gallery.css" type="text/css" />';
-      html += '</head><body>';
-		html += '<h1>Gallery</h1>';
-      html += gHtml;
-      html += '<h1>Hai</h1> Time is  ' + Date.now();
-		html += '</body></html>';
-		return html;
+  var html =  '<!doctype html>';
+      html += '<head>';
+      html +=   '<title>' + config.title + '</title>';
+      html +=   '<link href="gallery.css" rel="stylesheet" type="text/css">'
+      html += '</head>';
+      html += '<body>';
+      html += '  <h1>' + config.title + '</h1>';
+      html += '  <form method="GET" action="">';
+      html += '    <input type="text" name="title">';
+      html += '    <input type="submit" value="Change Gallery Title">';
+      html += '  </form>';
+      html += imageNamesToTags(imageTags).join('');
+      html += ' <form action="" method="POST" enctype="multipart/form-data">';
+      html += '   <input type="file" name="image">';
+      html += '   <input type="submit" value="Upload Image">';
+      html += ' </form>';
+      html += '</body>';
+  return html;
 }
 
 // Serve the gallery page
@@ -90,6 +94,61 @@ function serveGallery(req, res){
 			res.setHeader('Content-Type', 'text/html');
 			res.end(buildGallery(imageNames));			
 		});
+}
+
+function uploadImage(req, res) {
+  multipart(req, res, function(req, res) {
+    // make sure an image was uploaded
+    console.log('filename', req.body.filename)
+    if(!req.body.image.filename) {
+      console.error("No file in upload");
+      res.statusCode = 400;
+      res.statusMessage = "No file specified"
+      res.end("No file specified");
+      return;
+    }
+    fs.writeFile('images/' + req.body.image.filename, req.body.image.data, function(err){
+      if(err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.statusMessage = "Server Error";
+        res.end("Server Error");
+        return;
+      }
+      serveGallery(req, res);
+    });
+  });
+}
+
+function handleRequest(req, res) {
+  // at most, the url should have two parts -
+  // a resource and a querystring separated by a ?
+  var urlParts = url.parse(req.url);
+
+  if(urlParts.query){
+    var matches = /title=(.+)($|&)/.exec(urlParts.query);
+    if(matches && matches[1]){
+      config.title = decodeURIComponent(matches[1]);
+      fs.writeFile('config.json', JSON.stringify(config));
+    }
+  }
+
+  switch(urlParts.pathname) {
+    case '/':
+    case '/gallery':
+      if(req.method == 'GET') {
+        serveGallery(req, res);
+      } else if(req.method == 'POST') {
+        uploadImage(req, res);
+      }
+      break;
+    case '/gallery.css':
+      res.setHeader('Content-Type', 'text/css');
+      res.end(stylesheet);
+      break;
+    default:
+      serveImage(req.url, req, res);
+  }
 }
 
 var server = http.createServer((req, res) =>
@@ -113,7 +172,11 @@ var server = http.createServer((req, res) =>
 	};
 
 });
-server.listen(port, () =>
+/*server.listen(port, () =>
 {
 	console.log('Listening on port ' + port);
+});*/
+var server = http.createServer(handleRequest);
+server.listen(port, function(){
+	console.log('Server is listening on port ', port);
 });
